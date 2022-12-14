7e319dcbae820bb257822ffc249981552d65b75e
Serra <serratalay1@gmail.com>
2022-12-15 01:09:36 +0300

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Cover from './cover/Cover.jsx'
-import Login from './login/Login.jsx'
-import SignUp from './signup/SignUp.jsx'
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Cover from './Cover.jsx'
+import Login from './Login.jsx'
+import SignUp from './SignUp.jsx'
+import Welcome from './Welcome.jsx'
+import Branch from './Branch.jsx'
+import NewEvent from "./NewEvent.jsx";
+import Room from './Room.jsx'
+import Profile from './Profile.jsx'
 
 export default function App() {
   return (
@@ -12,6 +15,11 @@ export default function App() {
         <Route exact path="/" element={<Cover />} />
         <Route exact path="login" element={<Login />} />
         <Route exact path="signup" element={<SignUp />} />
+        <Route exact path="welcome" element={<Welcome />} />
+        <Route exact path="branch" element={<Branch />} />
+        <Route exact path="newevent" element={<NewEvent />} />
+        <Route exact path="room" element={<Room />} />
+        <Route exact path="profile" element={<Profile />} />
       </Routes>
   )
 }
