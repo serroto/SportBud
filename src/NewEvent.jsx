5eabd330a0c6130aa9571236a197d65c3e167115
Serra <serratalay1@gmail.com>
2022-12-16 01:41:38 +0300

    Creating new event updated

--- src/NewEvent.jsx
+++ src/NewEvent.jsx
@@ -5,6 +5,14 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { TimePicker } from 'antd';
 import dayjs from 'dayjs';
+import PlacesAutocomplete from 'react-places-autocomplete';
+import {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
+import reactElementToJSXString from 'react-element-to-jsx-string';
 
 export default function NewEvent(){
     const [selectedImage, setSelectedImage] = useState(null);
@@ -12,6 +20,26 @@ export default function NewEvent(){
     const [startDate, setStartDate] = useState(new Date());
     const format = 'HH:mm';
 
+    const[address,setAdress] = useState();
+    const [coordinates, setCoordinates] = useState({
+        lat:null,
+        lng:null
+    })
+    const handleSelect = async value=>{
+        const results = await geocodeByAddress(value);
+        const ll = await getLatLng(results[0])
+        // you can pass ll
+        setAdress(value)
+        setCoordinates(ll)
+    }
+
+    const [eventDate, setEventDate] = useState();
+
+    const setEventDateF = async value=>{
+        setEventDate(value);
+    }
+
+    const [title, setTitle] = useState('');
     return(
         <div className="create-new-event">
             <nav className='new-event-nav'>
@@ -26,13 +54,49 @@ export default function NewEvent(){
                 
             <div className="maps-gray">
             <i className="bi bi-geo-alt pin-icon"></i>
-            {/* <span>Choose Location</span> */}
-            <div id='map-itself'></div>
-            
+            <PlacesAutocomplete
+        value={address}
+        onChange={setAdress}
+        onSelect={handleSelect}
+        className="location-picker"
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div key={suggestions.description}>
+            <input
+              {...getInputProps({
+                placeholder: 'Place',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading..</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
             </div>
             <div className='date-filter-gray'>
             <i className="bi bi-calendar-check calendar-icon"></i>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="date-picker"/>
+            <DatePicker selected={startDate} value={eventDate} onChange={(date) => {setStartDate(date);setEventDate();}} onSelect={setEventDateF} className="date-picker"/>
             <TimePicker defaultValue={dayjs('13:30', format)} format={format} className="time-picker"/>
             </div>
 
@@ -53,7 +117,7 @@ export default function NewEvent(){
             </div>
 
             <div className='room-name'>
-            <span>Room's Title</span> <input placeholder='Enter Title'></input>
+            <span>Room's Title</span> <input placeholder='Enter Title' onChange={event => setTitle(event.target.value)}></input>
             </div>
 
             <div className='room-slogan'>
@@ -86,9 +150,9 @@ export default function NewEvent(){
             />
                 <img src="src/assets/yoga-room-pic.png" alt="" className='yoga-room-pic' onClick={(event)=> {const oldPic = oldRoomPic.current; oldPic.click(); event.currentTarget.classList.add("hide")} }/>
                 <div>
-                <p>Yaşam Parkı</p>
-                <p><i className="bi bi-geo-alt pin-icon white-icon"></i> 4 KM</p>
-                <p><i className="bi bi-clock clock-icon white-icon"></i> December 8, 8 PM</p>
+                <p>{title}</p>
+                <p><i className="bi bi-geo-alt pin-icon white-icon"></i>{address}</p>
+                <p><i className="bi bi-clock clock-icon white-icon"></i>{JSON.stringify(eventDate)}</p>
                 </div>
             </div>
             </div>
