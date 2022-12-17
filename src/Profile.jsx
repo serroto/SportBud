b2bbf81903da405a82715f79bbd9c58ff80013bb
Serra <serratalay1@gmail.com>
2022-12-17 15:53:18 +0300

    Profile Page redesigned

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,13 +3,34 @@ import { Link } from 'react-router-dom'
 export default function Profile(){
     return(
         <div className="profile">
-          <img src="src/assets/profile-page-ss.png" alt="" />
-          {/* <nav className='profile-nav'>
+          <nav className='profile-nav'>
           <Link to="/"><img src="src/assets/logo.png" alt="logo" className='logo'/></Link>
+          <span className='nav-links light'>
+            <Link to="/welcome">Home</Link>
+            <Link to="/profile">Profile</Link>
+          </span>
           </nav>
-          <div className='profile-img'>
-            <img src="src/assets/profile-pic.png" alt="" />
-          </div> */}
+          <img className='profile-img' src="src/assets/profile-pic.png" alt="" />
+          <div className='profile-info'>
+            <h4 className='user-name-title'>SERRA ATALAY</h4>
+            <div className='about'>
+              <h6 className='about-title'>ABOUT</h6>
+              <p>Hi, I'm Serra. I professionally do pilates and i like to do group runs.</p>
+            </div>
+            <div className='fav-branches'>
+            <h6 className='fav-branches-title'>FAVORITE BRANCHES</h6>
+              <img src="src/assets/people-yoga.png" alt="" />
+              <img src="src/assets/people-running.png" alt="" />
+            </div>
+            <div className='ratings'>
+              <div className='ratings-numbers'><span>8.7</span><span>8.7</span><span>9.4</span></div>
+              <div className='ratings-areas'><span>Loyalty</span><span>Fair Play</span><span>Performance</span></div>
+            </div>
+            <div className='events-created'>
+              <p>17/30 Succesful events created</p>
+            </div>
+
+          </div>
         </div>
     );
 }
